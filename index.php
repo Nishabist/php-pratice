512e369bd33d4d08b7196d4d0fa0dd422776f407
Nisha <nishabist27@gmail.com>
2023-05-30 08:03:21 +0545

    b

--- index.php
+++ index.php
@@ -1,9 +1,5 @@
 <?php
-session_start();
-if(!isset($_SESSION['id'])){
-    header('Location:login.php');
-    exit();
-}
+
 
 if(isset($_GET['success'])){
     echo'<h2 style=" border:1px solid blue;color:blue;padding:5px;text-align:center;">'.$_GET['success'].'</h2>';
@@ -12,12 +8,13 @@ require_once 'database_connect.php';
 $sql="SELECT * from student";
 $result =$conn->query($sql);
 
+
 if($result->num_rows>0){
     //output data from each row
-    echo '<a href="insert.php">Add Record</a><br>';
-    echo'<a href="logout.php">Logout</a>';
+    echo'<a href="login.php" >Admin Login</a>';
+    echo' <h1>Student Record</h1>';
     echo'<table cellspacing="10px" cellpadding="10px">';
-    echo '<tr><th>S.N</th><th>Name</th><th>Program</th><th>Address</th><th>Rollno</th><th>email</th><th>Action</th><tr>'
+    echo '<tr><th>S.N</th><th>Name</th><th>Program</th><th>Address</th><th>Rollno</th><th>email</th><tr>'
     ;
     $i=1;
     while($row=$result->fetch_assoc()){
@@ -26,8 +23,7 @@ if($result->num_rows>0){
         echo'<td>'.$row["email"].'</td>';
         echo'<td>'.$row["address"].'</td>';
         echo'<td>'.$row["rollno"].'</td>';
-        echo'<td><a href="update.php?id='.$row["id"].'">'.'Update</a>
-        <a href="delete.php?id='.$row["id"].'">'.'Delete</a></td>';
+       
         echo'</tr>';
         $i++;
 
@@ -44,8 +40,9 @@ $conn->close();
     a{
         text-decoration:none;
         color:black;
-        border:1px solid black;
         
+        display:flex;
+        justify-content: center;        
         
     }
     a:hover{
@@ -57,3 +54,8 @@ $conn->close();
         background:brown;
         color:#ffff;
     }
+    h1{
+        display:flex;
+        justify-content:center;
+    }
+    
